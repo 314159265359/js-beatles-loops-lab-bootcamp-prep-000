a08edfe1dad2b32754a62a1dde9339812716cfda
Karen G <kgraves0701@gmail.com>
2017-10-16 22:47:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPay(musicians,instruments) {
   var newArray = {}
-  for(i=0; i<musicians.length, i++){
+  for(i=0; i<musicians.length; i++){
     newArray.push(musicians[i] + " plays " + instruments[i])
   }
   return newArray
