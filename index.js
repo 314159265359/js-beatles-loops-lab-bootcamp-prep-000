3be8110dbf4f2328a0cfcbc2be922d008353848b
Karen G <kgraves0701@gmail.com>
2017-10-16 22:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPay(musicians,instruments) {
+function theBeatlesPlay(musicians,instruments) {
   var newArray = {}
   for(i=0; i<musicians.length; i++){
     newArray.push(musicians[i] + " plays " + instruments[i])
